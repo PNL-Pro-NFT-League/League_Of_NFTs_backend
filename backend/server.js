9b2eb254b2c5db2604bcd778ec76143d222258fa
Arka <arkadatta2002@gmail.com>
2022-08-21 10:59:57 +0530

    changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -31,18 +31,18 @@ app.use("/leaderboard", require("./routes/leaderboard"));
 app.use("/trading", require("./routes/trading"));
 app.use("/profile", require("./routes/profile"));
 
-// ------------------------------------Deployment --------------------------------
+// // ------------------------------------Deployment --------------------------------
 
-const __dirname1 = path.resolve()
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname1,'/frontend/build')))
-  app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname1,'frontend','build','index.html'));
-  })
+// const __dirname1 = path.resolve()
+// if(process.env.NODE_ENV === 'production'){
+//   app.use(express.static(path.join(__dirname1,'/frontend/build')))
+//   app.get('*', (req,res) => {
+//     res.sendFile(path.resolve(__dirname1,'frontend','build','index.html'));
+//   })
 
-}else{
-  app.send("API is Running Successfully!")
-}
+// }else{
+//   app.send("API is Running Successfully!")
+// }
 
 // ------------------------------------Deployment --------------------------------
 
